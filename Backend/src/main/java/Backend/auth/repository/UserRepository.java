c73b04420c9fd2ec4f200f95fb568dd3a696aa35
Idusara <126001751+Idusara@users.noreply.github.com>
2026-04-23 22:27:11 +0530

    feat: implement database seeder for users, resources, and bookings alongside UserRepository definition

--- Backend/src/main/java/Backend/auth/repository/UserRepository.java
+++ Backend/src/main/java/Backend/auth/repository/UserRepository.java
@@ -9,11 +9,8 @@ import java.util.Optional;
 
 public interface UserRepository extends MongoRepository<User, String> {
     Optional<User> findByEmail(String email);
-<<<<<<< Updated upstream
     Optional<User> findByUsername(String username);
     List<User> findByRolesContaining(Role role);
-=======
     boolean existsByEmail(String email);
     List<User> findAllByEmail(String email);
->>>>>>> Stashed changes
 }
